d17fc40159bdd8adfc47a4fcb417c3047f882409
specv <61646426+specv@users.noreply.github.com>
2022-12-30 18:25:15 +0800

    2. Agent: use `setup` callbacks in unit test

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,16 +1,19 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
 
-  test "delete values by key" do
-    {:ok, bucket} = KV.Bucket.start_link([])
+  test "delete values by key", %{bucket: bucket} do
     assert KV.Bucket.delete(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
