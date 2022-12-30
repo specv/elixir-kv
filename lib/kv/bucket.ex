831f39b9be82c9a8a595fb0372303315bab24dc5
specv <61646426+specv@users.noreply.github.com>
2022-12-30 18:20:48 +0800

    2. Agent: `KV.Bucket` implementation

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,35 @@
+defmodule KV.Bucket do
+
+  # calling use generates a `child_spec` function with default configuration
+  use Agent
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, fn map -> Map.get(map, key) end)
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, fn map -> Map.put(map, key, value) end)
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, fn map -> Map.pop(map, key) end)
+  end
+end
