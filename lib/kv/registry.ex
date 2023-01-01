75f528f2cea0b228276764f9f624c147428089ba
specv <61646426+specv@users.noreply.github.com>
2023-01-01 17:31:13 +0800

    3. GenServer: KV.Registry client side

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,6 +1,29 @@
 defmodule KV.Registry do
   use GenServer
 
+  @doc """
+  Starts the registry.
+  """
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Ensures there is a bucket associated with the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
   @impl true
   def init(:ok) do
     {:ok, %{}}
