83371e77893a971b6e379ffa02b4fd8271e91e9c
specv <61646426+specv@users.noreply.github.com>
2023-01-01 17:53:02 +0800

    3. GenServer: use `start_supervised!` instead of `start_link` in unit test

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -2,7 +2,13 @@ defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
   setup do
-    {:ok, registry} = KV.Registry.start_link([])
+    # The start_supervised! function was injected into our test module by use ExUnit.Case.
+    # It does the job of starting the KV.Registry process, by calling its start_link/1 function.
+    # The advantage of using start_supervised! is that ExUnit will guarantee that the registry
+    # process will be shutdown before the next test starts. In other words, it helps guarantee
+    # that the state of one test is not going to interfere with the next one in case they depend
+    # on shared resources.
+    registry = start_supervised!(KV.Registry)
     %{registry: registry}
   end
 
